a10af4a631647baf64b770c9946e1725dcdec6d7
Julia <julia.ryanx@gmail.com>
2015-05-09 13:38:07 +1000

    add video and lyric event listeners

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+// Video
+var video = document.getElementById("video");
 
+// Buttons
+var playButton = document.getElementById("play-pause");
+
+var resetButton = document.getElementById("revert");
+// function stop() {
+//  var pauseButton = document.getElementsByTagName('audio')[0];
+//   video.pause();
+//   video.currentTime = 0;
+// }
+
+$(document).ready(function() {
+  // Event listener for the play/pause button
+  playButton.addEventListener("click", function() {
+     video.play();
+  });
+
+  resetButton.addEventListener("click", function() {
+    video.pause();
+    video.currentTime = 0;  
+  })
+});
