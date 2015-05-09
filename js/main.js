69c53a6079f1f31e36e4ed4b2a5c3697c4a2e6c3
soxface <sonyacorcoran@gmail.com>
2015-05-09 18:23:45 +1000

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Video
+var video = document.getElementById("video");
+
+// Buttons
+var playButton = document.getElementById("play-pause");
+
+var resetButton = document.getElementById("revert");
+// function stop() {
+//  var pauseButton = document.getElementsByTagName('audio')[0];
+//   video.pause();
+//   video.currentTime = 0;
+// }
+
+$(document).ready(function() {
+  // Event listener for the play/pause button
+  playButton.addEventListener("click", function() {
+     
+     setTimeout(function() {
+      video.play(); 
+     }, 3000);
+  });
+
+  resetButton.addEventListener("click", function() {
+    video.pause();
+    video.currentTime = 0;  
+  })
+});
